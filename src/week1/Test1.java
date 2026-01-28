1de6d2d8fb02422258a20b17e921c3475fef609f
Joshua B. Gross <gross.joshua.b@gmail.com>
2026-01-28 11:22:37 -0800

    code from first week

---
+++ src/week1/Test1.java
@@ -0,0 +1,36 @@
+package week1;
+
+public class Test1 {
+    public static void main(String[] args) {
+        // create an integer variable called x
+        // and assign it the value of 3
+        int x = 3;
+        // y = 7;
+        x = 5;
+        int y = 14;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(x + y);
+        System.out.println(x);
+        int z = x + y;
+        double u = 5.74129;
+        double v = 1.3;
+        double w = u + v;
+        System.out.println(w);
+        System.out.println(u);
+        System.out.println(w);
+        double h = v * x;
+        System.out.println(h);
+
+        double i = x - 3.0;
+        i = 6.0;
+        double k = 1.0 / 3.0;
+        System.out.println(k);
+        double l = 1  / 3 ;
+        int m = 7 / 3;
+        System.out.println(m);
+        int n = 7 % 3;
+        System.out.println();
+
+    }
+}
---
+++ src/week1/Test2.java
@@ -0,0 +1,26 @@
+package week1;
+
+public class Test2 {
+    public static void main(String[] args) {
+         int x = 3;
+         System.out.println(x);
+         x = 5;
+         System.out.println(x);
+         int y = 14;
+         System.out.println(x+y);
+         int z = x + y;
+         double h = 0.000000005;
+         double j = 50000.2;
+         System.out.println(j);
+         System.out.println(j + x);
+         double k = j + x;
+         double l = j * 2;
+         System.out.println(l);
+         double m = 1./3.0;
+         System.out.println(m);
+         double o = 1/3;
+         System.out.println(o);
+         int q = 1/3;
+         int r = 1 % 3; 
+    }
+}
