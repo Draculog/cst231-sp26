edb7e97b0b219fdee4171d318afd45f7af9ae060
Joshua B. Gross <gross.joshua.b@gmail.com>
2026-01-28 17:27:15 -0800

    code from first week

---
+++ src/week2/Wed_Sec2.java
@@ -0,0 +1,18 @@
+package week2;
+
+public class Wed_Sec2 {
+    public static void main(String[] args) {
+        int a = 42;
+//        int a = 43;
+        a = 43;
+        double u = 64;
+        System.out.println(u);
+        boolean pepsi = true;
+        String name = "Handy Andy";
+        int p = 14;
+        int q = -15;
+        boolean b1 = p%2 == 0;
+        boolean b2 = q%2 == 1;
+        // turing
+    }
+}
