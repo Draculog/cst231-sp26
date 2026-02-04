c76d86e1d8e9b6d6cc781ef80e2bd43c9f3f8f5a
Joshua B. Gross <gross.joshua.b@gmail.com>
2026-02-04 10:54:53 -0800

    wed week 3 am code

---
+++ src/week3/Wed_Sec1.java
@@ -0,0 +1,40 @@
+package week3;
+
+import java.util.Random;
+
+public class Wed_Sec1 {
+    public static void main(String[] args) {
+//        Random r = new Random(-7);
+//        int x = r.nextInt();
+//        System.out.println(x);
+//        x = Math.abs(x);
+//        System.out.println(x);
+//        x = x % 10;
+//        System.out.println(x);
+
+        // code structure
+        // control structure
+        // conditional - make decisions - choose - select
+        int x = 7;
+//        if(5 < x < 7) {
+//        if(x > 5) && (x < 7){
+        if((x > 5) && (x < 7)){
+        }
+        System.out.println("start");
+        if(false) {
+            System.out.println("hello");
+        } else if (false ){
+            System.out.println("goodbye");
+        }
+        System.out.println("stop");
+
+//        System.out.println(r.nextInt()%100);
+//        System.out.println(r.nextInt()%100);
+//        System.out.println(r.nextInt()%100);
+//        System.out.println(r.nextInt()%100);
+//        System.out.println(r.nextInt()%100);
+//        System.out.println(r.nextInt()%100);
+//        System.out.println(r.nextInt()%100);
+//        System.out.println(r.nextInt()%100);
+    }
+}
