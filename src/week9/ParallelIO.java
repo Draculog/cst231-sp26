e9d02aad24fde1b433d9d9bcb79eddf24a563f24
Joshua B. Gross <gross.joshua.b@gmail.com>
2026-03-16 11:12:33 -0700

    continued parallel writing

--- src/week9/ParallelIO.java
+++ src/week9/ParallelIO.java
@@ -11,11 +11,30 @@ public class ParallelIO {
         FileWriter fTeam = new FileWriter("src/week9/" + teamFile);
         PrintWriter pTeam = new PrintWriter(fTeam);
 
+        FileWriter fMember = new FileWriter("src/week9/" + memberFile);
+        PrintWriter pMember = new PrintWriter(fMember);
+
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter team name: ");
+        System.out.print("Enter team name (0 to stop): ");
         String teamName = in.next();
-        pTeam.println(teamName);
+
+        while(! teamName.equals("0")) {
+            pTeam.println(teamName);
+            System.out.print("Enter team size: ");
+            int size = in.nextInt();
+            pTeam.println(size);
+            for (int i = 0; i < size; i++) {
+                System.out.print("Enter team member " + (i + 1) + ": ");
+                String member = in.next();
+                pMember.println(member);
+            }
+
+            System.out.print("Enter team name (0 to stop): ");
+            teamName = in.next();
+
+        }
 
         fTeam.close();
+        fMember.close();
     }
 }
